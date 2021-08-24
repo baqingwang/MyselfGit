c1423e01e6b67f420397e082e496190d76575a38
baqingwang <906068821@qq.com>
2021-08-24 22:29:53 +0800

    项目初始结构提交

---
+++ gulimall-order/src/test/java/com/baqw/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baqw.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/baqw/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baqw.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
